a94de937e8ab6aa3081bc74ecdbdb76976029b59
Cobbleston <canovasimo@gmail.com>
2022-10-03 12:39:30 +0200

    first two lectures

---
+++ script.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/bash
+
+D=${1:-$(date +%Y%m%d)}
+
+mkdir 20220929 && cd ./20220929
+
+T=$(mktemp)
+
+URL="http://soho.cs.unibo.it:8888/$D"
+
+echo "URL scelto: "$URL
+echo "TMP file: "$T
+
+curl -Ls $URL | grep  href | awk '{ print $2 }' | grep -o '".*"' | tr -d '"' > $T
+
+cat $T | while read line; do
+	K=$URL/$line
+	wget -q $K
+done
+
+#http://soho.cs.unibo.it:8888/20220929/
+# wget -nH -r --cut-dirs=10 --no-parent --reject "index.html*"  http://soho.cs.unibo.it:8888/20220929/
