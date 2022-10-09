38366d15d60f345e968a223b4b63c00604a17c22
Cobbleston <canovasimo@gmail.com>
2022-10-09 17:55:09 +0200

    .

---
+++ 20221006/struct.c
@@ -0,0 +1,20 @@
+// Esempi di modi per scrivere le strutture
+struct coord {
+	int x;
+	int y;
+};
+
+struct coord vertex;
+
+struct {
+	char *name;
+	char *surname;
+} complete_name;
+
+typedef struct {
+	char *item;
+	long price;
+} shop_item;
+
+shop_item book;
+
